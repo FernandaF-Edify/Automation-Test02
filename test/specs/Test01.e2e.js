69eecc2ede0419c0e5e725fe1977f8e267575ea1
Mar Fernndez <fernanda.fernandez@edify.cr>
2020-08-26 23:41:57 -0600

    Chai integretion

--- test/specs/Test01.e2e.js
+++ test/specs/Test01.e2e.js
@@ -22,46 +22,6 @@ describe('Automation Test - Module 2 - Test  01', () => {
 
     })
 
-    before (()=>{
-        FlightKeyword.inicializBookingPage();
-    })
-
-    it ('Should search flight with train transport', ()=> {
-        expect(browser).toHaveUrlContaining('/search');
-        expect(FlightKeyword.isFlightOnSearchPage());
-        FiltersPage.SelectOnlyBus();
-        FiltersPage.FlightErrorFound.waitForExist();
-        chaiExpect(FiltersPage.isAnyFlightExist()).to.be.true;
-        FiltersPage.TraindCheckBox.click();
-        expect(FlightKeyword.isFlightOnSearchPage());
-    })
-
-    it ('Should book a flight to TXL to LONDON', ()=> {
-        BookFlightPage.btnBook.click();
-        BookFlightPage.title.waitForExist();
-        chaiExpect(BookFlightPage.isStartBooking()).to.be.true;
-        BookFlightPage.FillContactDetails();
-
-        flightFirstNameDetails = BookFlightPage.fillFirstName.getAttribute('value');
-        flightFirstLastDetails = BookFlightPage.fillLastName.getAttribute('value');
-
-        chaiExpect(BookFlightPage.isStartFillPrimaryPassegers()).to.be.true;
-        BookFlightPage.FillPrimaryPassenger();
-        chaiExpect(BookFlightPage.isVisaRequiremets()).to.equal(true);
-        
-        BookFlightPage.FillVisaRequirements();
-        BookFlightPage.btnContinue.click();
-        BookFlightPage.btnStandardType.click();
-        BookFlightPage.btnPackagesPremium.click();
-        BookFlightPage.btnSelectWindow.click();
-        BookFlightPage.btnStepControlNxt.click();
-
-        BookFlightPage.emailDetails.isEqual('Ms' + flightFirstNameDetails + flightFirstLastDetails);
-
-        expect(browser).to.include('/booking?');
-        
-    })
-
     after(()=>{
         browser.closeWindow();
     })
